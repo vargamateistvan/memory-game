dd0d5aef247e86fd961c366aa4171ad8ffabc9a0
MateIstvanVarga <mateistvanvarga@gmail.com>
2023-04-17 12:01:48 +0200

    Setup

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingCommas: "all",
+  bracketSpacing: true,
+  bracketLine: false,
+  arrowParens: "always",
+};
